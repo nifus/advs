7639e5bb12884e33e84a840f7967abb5ac1d52a4
a.bunyza <a.bunyza@gmail.com>
2016-09-28 09:26:24 +0300

    add user seed

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+
+
+            User::create([
+                'name'=>'Alex',
+                'email'=>'a.bunzya@gmail.com',
+                'is_activated'=>'1',
+                'group_id'=>'2',
+                'surname'=>'Bunzya',
+                'sex'=>'male',
+                'password'=>'testpass',
+                'activate_key'=>1
+            ]);
+
+    }
+}
