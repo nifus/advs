b0cec9919367dbc31238ebd04698822ca5b50b46
Alexander Bunzya <a.bunzya@gmail.com>
2016-11-06 21:36:06 +0300

    add de

---
+++ resources/lang/de/main.php
@@ -0,0 +1,244 @@
+<?php
+
+return [
+        //  footer menu
+    'home' => 'Home',
+    'contacts' => 'Contact',
+    'AGB' => 'AGB',
+    'disclaimer' => 'Impressum',
+
+        //  main menu
+    'rent'=>'Rent',
+    'buy'=>'Buy',
+    'offer'=>'Offer',
+
+        //  auth section
+    'email'=>'Email',
+    'password'=>'Password',
+    'forgot_password'=>'Forgot password',
+    'remember_me'=>'Remember me',
+    'login'=>'Login',
+    'logout'=>'Logout',
+    'register'=>'Register',
+    'address'=>'Anschrift',
+    'agb-head'=>'Allgemeine Geschäftsbedingungen (AGB)',
+    'profile' => 'Profile',
+
+
+        //  reset pass page
+    'password_reset'=>'Password Reset',
+    'password_reset_invalid_message'=>'Invalid password reset',
+    'password_reset_complete_message'=>'Password reset is complete',
+    'password_reset_sent_message'=>'We sent to you email new credentials.',
+
+        // activation account
+    'account_activation' => 'Account activation',
+    'activation_invalid_message'=>'Invalid account activation',
+    'activation_complete_message'=>'Activate account is complete ',
+    'activation_complete_message_details'=>'Now you can enjoy all the features of our website',
+
+
+        //  register page
+    'register_header'=> 'Register Private Account',
+    'register_complete'=>'Thank you for registration.',
+    'register_complete_desc'=>'We have send an email to your email address.<br>
+                Please click on the activation link inside this email.<br>
+                Otherwise you are not able to login to your account.<br>
+                This activation link is valid for the next 24 hours.<br>',
+    'register_private_account_message'=>'If you are a real estate agent / agency please create a <a
+                            href=":link">business account</a>',
+    'register_private_header' =>'Account type: PRIVATE',
+    'register_title'=>'Title',
+    'register_man'=>'Mister',
+    'register_woman'=>'Miss',
+
+
+    'register_forename'=>'Forename',
+    'register_surname'=>'Surname',
+    'register_email'=>'Email address',
+    'register_email_re'=>'Repeat email address',
+    'register_password'=>'Password',
+    'register_password_length'=>'6 length minimum',
+    'register_password_re'=>'Repeat password',
+    'register_captcha'=>'Captcha validate',
+    'register_agb_accept'=>'AGB accept?',
+    'register_create_account_button'=>'Create Account',
+    'register_private_desc'=>'A private account provide you the following advantages:
+                <ul>
+                    <li>No hidden costs, the useage is free!</li>
+                    <li>Creation advertisements</li>
+                    <li>Save used car advertisements to your personal watchlist</li>
+                </ul>',
+
+
+        //  business register page
+    'register_business_header'=> 'Register Business Account',
+    'business_header'=> 'Business Account',
+    'register_business_complete_desc'=>'We have send an email to your email address.<br>
+                Please click on the activation link inside this email.<br>
+                Otherwise you are not able to login to your account.<br><br>
+
+                <strong>After you click on this required
+                activation
+                can and start
+                the are
+                activation
+                process for you.</strong>
+                This link
+                email we
+                address
+                password
+                later your login
+                This process takes one or two credentials<br><br>
+
+                This activation link is valid for the next 24 hours.<br>',
+    'register_business_account_message'=>'If you are NOT a real estate agent / agency please create a <a
+                            href=":link">private account</a>',
+
+    'register_company_data'=>'Company Data',
+    'register_company_name'=>'Company name',
+    'register_company_vat'=>'Please enter your legal company name according to
+                                your VAT ID confirmation.',
+    'register_company_register_id'=>'Commercial register ID',
+    'register_company_website'=>'Website',
+    'register_company_address'=>'Address',
+    'register_company_address_related_vat'=>'Please enter the company data which is identical to
+                                the address on your official VAT ID registration
+                                document',
+    'register_company_street'=>'Street',
+    'register_company_no'=>'No.',
+    'register_company_zip'=>'Zip',
+    'register_company_city'=>'City',
+    'register_company_add_addr'=>'Additional address',
+    'register_company_country'=>'Country',
+    'register_company_login'=>'Login credentials',
+    'register_company_email_details'=>'This email address and password are later your login
+                        credentials',
+    'register_company_contact'=>'Contact person',
+    'register_company_manager'=>'Please enter here the general manager of the
+                        company. <br>
+                        Later you can assign an other contact person which is
+                         visible for all users in the account settings',
+
+    'register_company_personal_email'=>'This is the personal email address of the contact
+                                person (not the login email address)',
+    'register_company_phone'=>'Phone',
+    'register_company_subsc'=>'Subscription',
+    'register_company_select_subsc'=>'Select you subscription',
+    'register_company_package'=>'Package',
+    'register_company_slots_per_month'=>'Slots per month',
+    'register_company_price_per_month'=>'Price per month',
+    'register_company_each_extra_slots'=>'Each extra slot',
+    'register_company_payment'=>'Payment',
+    'register_company_select_payment'=>'Select your Payment',
+    'register_company_paypal'=>'PayPal',
+    'register_company_paypal_desc'=>'(you will be forwarded to PayPal by clicking on "Next")',
+    'register_company_paypal_email'=>'PayPal email',
+    'register_company_giropay'=>'GiroPay',
+    'register_company_giropay_desc'=>'(you will be forwarded to you Bank)',
+    'register_company_giropay_acc'=>'GiroPay account',
+    'register_company_prepayment'=>'Prepayment',
+    'register_company_finish'=>'Finish',
+    'register_company_desc'=>'The business account provide you the following advantages:
+                <ul>
+                    <li>Creation of advertisements</li>
+                    <li>Be able to subscripe a service</li>
+                    <li>Save used car advertisements to your personal watchlist</li>
+                </ul>
+
+
+                <br>
+                The following payment methods are available:
+                <ul>
+                    <li>Lastschrift</li>
+                    <li>PayPal</li>
+                    <li>Credit Card</li>
+                </ul>',
+
+    'create_adv'=> 'Create advertisement',
+    'create_adv_need_register'=>'You need <a href=":link">
+                    create an account</a> or <a ng-click="displayLoginForm()" class="link">login</a>
+                to an existing account',
+    'create_adv_type'=>'Advert type',
+    'create_adv_select'=>'Please select your advertisement type',
+    'create_adv_rent'=>'Rent',
+    'create_adv_business'=>'Business realty',
+    'create_adv_sale'=>'Sale',
+    'create_adv_finish'=>'Finish',
+    'create_adv_not_fill_fields'=>'You did not fill in all
+                                    fields',
+    'create_adv_send'=>'Send',
+
+    'category_Flat'=>'Flat',
+    'category_House'=>'House',
+    'category_Garage / car space'=>'Garage / car space',
+    'category_Building ground'=>'Building ground',
+    'category_Office / Praxis'=>'Office / Praxis',
+    'category_Gastronomy / Hotel'=>'Gastronomy / Hotel',
+    'category_Hall / Production / Warehouse'=>'Hall / Production / Warehouse',
+    'category_Retail trade'=>'Retail trade',
+    'category_Commercial land'=>'Commercial land',
+
+    'equipment_Balcony/Terrace'=>'Balcony/Terrace',
+    'equipment_New building'=>'New building',
+    'equipment_Build-in kitchen'=>'Build-in kitchen',
+    'equipment_Garden (shared-use)'=>'Garden (shared-use)',
+    'equipment_Elevator'=>'Elevator',
+    'equipment_Garage/parking space'=>'Garage/parking space',
+    'equipment_Stepless access'=>'Stepless access',
+    'equipment_Guest toilet'=>'Guest toilet',
+    'equipment_Cellar'=>'Cellar',
+    'equipment_Kitchen available'=>'Kitchen available',
+    'equipment_High voltage'=>'High voltage',
+    'equipment_Crane runway'=>'Crane runway',
+    'equipment_Cold storage warehouse'=>'Cold storage warehouse',
+    'equipment_Terrace for guests'=>'Terrace for guests',
+    'equipment_Ramp'=>'Ramp',
+    'equipment_Hydraulic lift'=>'Hydraulic lift',
+    'equipment_Freight elevator'=>'Freight elevator',
+
+    'subcategory_Souterrain'=>'Souterrain',
+    'subcategory_Loft'=>'Loft',
+    'subcategory_Top floor flat'=>'Top floor flat',
+    'subcategory_Penthouse'=>'Penthouse',
+    'subcategory_Downstairs flat'=>'Downstairs flat',
+    'subcategory_Maisonette'=>'Maisonette',
+    'subcategory_Raised ground floor'=>'Raised ground floor',
+    'subcategory_Terrace flat'=>'Terrace flat',
+    'subcategory_Any'=>'Any',
+    'subcategory_Single-family house'=>'Single-family house',
+    'subcategory_Mid-terrace town house'=>'Mid-terrace town house',
+    'subcategory_End-terrace town house'=>'End-terrace town house',
+    'subcategory_Multi-family house'=>'Multi-family house',
+    'subcategory_Bungalow'=>'Bungalow',
+    'subcategory_Farmhouse'=>'Farmhouse',
+    'subcategory_Semi-detached house'=>'Semi-detached house',
+    'subcategory_Villa'=>'Villa',
+    'subcategory_Castle / Palace'=>'Castle / Palace',
+    'subcategory_Outside-parking space'=>'Outside-parking space',
+    'subcategory_Carport'=>'Carport',
+    'subcategory_Garage'=>'Garage',
+    'subcategory_Underground carpark'=>'Underground carpark',
+    'subcategory_Car park'=>'Car park',
+    'subcategory_Loft/Studio'=>'Loft/Studio',
+    'subcategory_Office'=>'Office',
+    'subcategory_Praxis'=>'Praxis',
+    'subcategory_Bar/Bistro/Cafe'=>'Bar/Bistro/Cafe',
+    'subcategory_Club/Disco'=>'Club/Disco',
+    'subcategory_Restaurant'=>'Restaurant',
+    'subcategory_Hotel'=>'Hotel',
+    'subcategory_Pension'=>'Pension',
+    'subcategory_Hall(+ open space)'=>'Hall(+ open space)',
+    'subcategory_Warehouse(+ open space)'=>'Warehouse(+ open space)',
+    'subcategory_Industrial building'=>'Industrial building',
+    'subcategory_Cold-storage(+ warehouse)'=>'Cold-storage(+ warehouse)',
+    'subcategory_Car workshop'=>'Car workshop',
+    'subcategory_Shopping center'=>'Shopping center',
+    'subcategory_Shop'=>'Shop',
+    'subcategory_Kiosk'=>'Kiosk',
+    'subcategory_Store'=>'Store',
+    'subcategory_Buildings'=>'Buildings',
+    'subcategory_Parking'=>'Parking',
+    'subcategory_Garden / Farming'=>'Garden / Farming',
+
+];
