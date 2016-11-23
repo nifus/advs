d92ceedfe21e05d4731c1b12b2a303ad959fff4c
Alexander Bunzya <a.bunzya@gmail.com>
2016-11-23 12:20:47 +0300

    update search result

--- public/apps/frontApp/adv/searchResult/searchResultController.js
+++ public/apps/frontApp/adv/searchResult/searchResultController.js
@@ -22,33 +22,20 @@
             current: [],
             display_map: false,
             map: null,
-            submit: false
-
+            submit: false,
+            rows: null
         };
         $scope.promises = [];
 
 
-        $scope.openAdv = function(adv){
-            window.location.href = "/search/"+$scope.env.result_id+"#/view"+adv;
-            $scope.env.adv_id = adv;
-            advFactory.getById($scope.env.adv_id).then( function(response){
-                $scope.adv=response;
-            });
-            initGoogleMaps();
-        };
-        $scope.changePerPage = function (value) {
+
+        $scope.changePerPage = function (value, page) {
             $scope.env.per_page = value * 1;
-            $scope.env.page = value * 1;
-            $scope.setPage(1);
+            if (page==undefined){
+                $scope.env.page = value * 1;
+                $scope.setPage(1);
+            }
             updateSearch();
-        };
-
-        function updatePagination() {
-            var pages = Math.round($scope.env.rows.length / $scope.env.per_page);
-            $scope.env.pages = pages > 1 ? pages : null;
-
-            $scope.env.current = $scope.env.rows.slice(($scope.env.page - 1) * $scope.env.per_page, (($scope.env.page - 1) * $scope.env.per_page) + $scope.env.per_page);
-            window.scrollTo(0, 0);
         };
 
         $scope.range = function (min, max, step) {
@@ -64,7 +51,6 @@
             window.location.href = "/search/" + $scope.env.result_id + "#/page" + page;
             $scope.env.page = page;
             updatePagination();
-
         };
 
         $scope.displayMap = function (flag, need_update) {
@@ -75,6 +61,41 @@
             if ($scope.env.display_map === true) {
                 initGoogleMaps()
             }
+        };
+
+        $scope.changeSort = function (value) {
+            $scope.env.sortby = value;
+            if (value == 'date_create') {
+                $scope.env.rows = $filter('orderBy')($scope.env.rows, '-created_at');
+            } else if (value == 'price_up') {
+                $scope.env.rows = $filter('orderBy')($scope.env.rows, '-cold_rent', true);
+            } else if (value == 'price_down') {
+                $scope.env.rows = $filter('orderBy')($scope.env.rows, '-cold_rent');
+            }
+            updatePagination();
+            updateSearch();
+        };
+
+        $scope.addToFav = function (adv, flag) {
+
+            if (flag === true) {
+                adv.addToFavList($scope.user);
+                alertify.success('Adv added to watchlist');
+            } else {
+                adv.deleteFromFavList($scope.user);
+                alertify.success('Adv removed from watchlist');
+            }
+        }
+
+        function updatePagination() {
+            if ($scope.env.rows==null){
+                return;
+            }
+            var pages = Math.round($scope.env.rows.length / $scope.env.per_page);
+            $scope.env.pages = pages > 1 ? pages : null;
+
+            $scope.env.current = $scope.env.rows.slice(($scope.env.page - 1) * $scope.env.per_page, (($scope.env.page - 1) * $scope.env.per_page) + $scope.env.per_page);
+            window.scrollTo(0, 0);
         };
 
         function updateSearch() {
@@ -101,29 +122,7 @@
 
         }
 
-        $scope.changeSort = function (value) {
-            $scope.env.sortby = value;
-            if (value == 'date_create') {
-                $scope.env.rows = $filter('orderBy')($scope.env.rows, '-created_at');
-            } else if (value == 'price_up') {
-                $scope.env.rows = $filter('orderBy')($scope.env.rows, '-cold_rent', true);
-            } else if (value == 'price_down') {
-                $scope.env.rows = $filter('orderBy')($scope.env.rows, '-cold_rent');
-            }
-            updatePagination();
-            updateSearch();
-        };
 
-        $scope.addToFav = function (adv, flag) {
-
-            if (flag === true) {
-                adv.addToFavList($scope.user);
-                alertify.success('Adv added to watchlist');
-            } else {
-                adv.deleteFromFavList($scope.user);
-                alertify.success('Adv removed from watchlist');
-            }
-        }
 
         ////
         $scope.favlist = function (flag) {
@@ -169,6 +168,34 @@
             })
         };
 
+        $scope.$watch(function(){return window.location.href},function (value) {
+
+            var url = value.match(/view([0-9]*)/);
+            $scope.env.adv_id = url != null && url[1] != undefined ? url[1] * 1 : null;
+
+            url = value.match(/\/search\/([0-9]*)/);
+            $scope.env.result_id = url != null && url[1] != undefined ? url[1] * 1 : null;
+
+            url = value.match(/\/page([0-9]*)/);
+            $scope.env.page = url != null && url[1] != undefined ? url[1] * 1 : $scope.env.page;
+
+
+            if( $scope.env.adv_id == null ){
+                initListing();
+            }else{
+                initView();
+            }
+        })
+
+        $scope.goBack = function () {
+            if ( $scope.env.rows ){
+                window.history.back()
+            }else{
+                window.location.href='/search/'+$scope.env.result_id+'#/page1';
+            }
+
+        }
+
         function initGoogleMaps() {
             var interval = $interval(function(){
                 if ( document.getElementById('map') ){
@@ -187,82 +214,66 @@
             $scope.message = data;
         }
 
+        //
+        function initListing() {
+            $scope.env.loading = true;
 
-        $scope.$watch(function(){return window.location.href},function (value) {
-            var url = value.match(/view([0-9]*)/);
-            if( url == null ){
-                $scope.env.adv_id = undefined
-            }
-        })
-
-        $scope.goBack = function () {
-
-            window.history.back()
-        }
-        function init() {
-            var url = window.location.href.match(/view([0-9]*)/);
-            $scope.env.adv_id = url != null && url[1] != undefined ? url[1] * 1 : null;
-
-            url = window.location.href.match(/\/search\/([0-9]*)/);
-            $scope.env.result_id = url != null && url[1] != undefined ? url[1] * 1 : null;
-
-            url = window.location.href.match(/\/page([0-9]*)/);
-            $scope.env.page = url != null && url[1] != undefined ? url[1] * 1 : $scope.env.page;
-            if ($scope.env.adv_id ){
-                $scope.openAdv($scope.env.adv_id)
-
+            if ( $scope.env.search==null ) {
                 var searchPromise = searchLogFactory.getById($scope.env.result_id).then(function (response) {
                     $scope.env.search = response;
                 });
                 $scope.promises.push(searchPromise);
+            }
 
+            if ( $scope.env.rows==null){
                 var advPromise = advFactory.getResult($scope.env.result_id, {}).then(function (response) {
                     $scope.env.rows = response.advs;
                     $scope.env.city = response.city;
                 });
                 $scope.promises.push(advPromise);
+            }
+
+            $q.all($scope.promises).then(function () {
+                $scope.env.loading = false;
 
 
+                $scope.setPage($scope.env.page);
 
-                $q.all($scope.promises).then(function () {
-                    $scope.env.loading = false;
-                });
+                if ($scope.env.search.config && $scope.env.search.config.per_page) {
+                    $scope.changePerPage($scope.env.search.config.per_page , $scope.env.page)
+                }
+                if ($scope.env.search.config && $scope.env.search.config.sortby) {
+                    $scope.changeSort($scope.env.search.config.sortby)
+                }
+                if ($scope.env.search.config && $scope.env.search.config.display_map) {
+                    $scope.displayMap($scope.env.search.config.display_map, false);
+                }
+            });
+        }
 
-            }else{
+        function initView() {
+            $scope.env.loading = true;
+
+            var advPromise = advFactory.getById($scope.env.adv_id).then( function(response){
+                $scope.adv=response;
+            });
+            $scope.promises.push(advPromise);
+
+            if ( $scope.env.search==null ){
                 var searchPromise = searchLogFactory.getById($scope.env.result_id).then(function (response) {
                     $scope.env.search = response;
                 });
                 $scope.promises.push(searchPromise);
-
-                var advPromise = advFactory.getResult($scope.env.result_id, {}).then(function (response) {
-                    $scope.env.rows = response.advs;
-                    $scope.env.city = response.city;
-                });
-                $scope.promises.push(advPromise);
-
-
-
-                $q.all($scope.promises).then(function () {
-
-                    $scope.env.loading = false;
-                    $scope.setPage($scope.env.page);
-
-                    if ($scope.env.search.config && $scope.env.search.config.per_page) {
-                        $scope.changePerPage($scope.env.search.config.per_page)
-                    }
-                    if ($scope.env.search.config && $scope.env.search.config.sortby) {
-                        $scope.changeSort($scope.env.search.config.sortby)
-                    }
-                    if ($scope.env.search.config && $scope.env.search.config.display_map) {
-                        $scope.displayMap($scope.env.search.config.display_map, false);
-                    }
-                });
+                initGoogleMaps();
             }
 
 
-
+            $q.all($scope.promises).then(function () {
+                $scope.env.loading = false;
+            });
         }
-        init();
+
+
     }
 })();
 
